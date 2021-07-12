1c34adb88eda2fcd23d7b523471bea9a40cde6a7
Yannasit <624259011@webmail.npru.ac.th>
2021-07-12 15:44:29 +0700

    change

--- src/com/swa/oop1/Product.java
+++ src/com/swa/oop1/Product.java
@@ -5,6 +5,7 @@ public class Product {
 	    private String productName;
 	    Product(String pid, String pname){
 	      productID = pid;
+	      productName = pname;
 	    }
 	    public String getDetail(){
 	      return "Product ID : "+productID+" and Product Name : "+productName;
